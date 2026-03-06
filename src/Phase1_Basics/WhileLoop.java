7e39ed1cc3e376e91342bd68ac7646ac5458cddf
sheeraz-H <xherazusain@gmail.com>
2026-03-07 01:21:37 +0500

    Add WhileLoop class to demonstrate basic while loop functionality

---
+++ src/Phase1_Basics/WhileLoop.java
@@ -0,0 +1,14 @@
+package src.Phase1_Basics;
+
+// While Loop (When you loop based on a condition):
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count < 9) {
+            System.out.println("While Count: " + count);
+            count++;
+        }
+    }
+}
